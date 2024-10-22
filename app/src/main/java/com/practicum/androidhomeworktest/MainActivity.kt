b3e6b95614c36e6954e0d5476eb44e68ded63901
artembelonozhko <ar.belonozhko@gmail.com>
2024-10-22 14:27:34 +0300

    add note layout added

--- app/src/main/java/com/practicum/androidhomeworktest/MainActivity.kt
+++ app/src/main/java/com/practicum/androidhomeworktest/MainActivity.kt
@@ -1,6 +1,9 @@
 package com.practicum.androidhomeworktest
 
+import android.content.Intent
 import android.os.Bundle
+import android.view.inputmethod.InputBinding
+import android.widget.Button
 import android.widget.LinearLayout
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
@@ -8,18 +11,34 @@ import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+import com.practicum.androidhomeworktest.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
 
 
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
+
+            val button: FloatingActionButton = findViewById(R.id.button_add_note)
+//        val button: Button = findViewById(R.id.button)
+
+        button.setOnClickListener {
+            val intent = Intent(this, AddNote::class.java)
+            startActivity(intent)
+        }
+
+
         val recyclerView: RecyclerView = findViewById(R.id.recyclerView)
         recyclerView.layoutManager = LinearLayoutManager(this)
         recyclerView.adapter = CustomRecyclerAdapter(fillList())
+
+
 
     }
     private fun fillList(): List<String> {
@@ -27,4 +46,6 @@ class MainActivity : AppCompatActivity() {
         (0..30).forEach { i -> data.add("$i element") }
         return data
     }
+
+
 }
